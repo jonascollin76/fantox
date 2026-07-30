fc77ec19b36d230607ebac201a864f4bed049d16
jonascollin76 <88001282+jonascollin76@users.noreply.github.com>
2026-07-30 23:44:16 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  turbopack: {
-    root: __dirname,
-  },
 }
-
 export default nextConfig
